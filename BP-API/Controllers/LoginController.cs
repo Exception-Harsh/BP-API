c653253325289020719ed8fb0b4fda2e427bd287
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2025-02-10 23:55:14 +0530

    BorrowerPortal Backend

---
+++ BP-API/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BP_API.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private Dictionary<string, string> users = new Dictionary<string, string>()
+        {
+            { "borrower1", "b123" },
+            { "borrower2", "b234" },
+            { "arbour", "a123" },
+            { "pme",  "p123" },
+            { "trustee", "t123" }
+        };
+
+        [HttpPost]
+        [Route("api/login")]
+        public HttpResponseMessage Login([FromBody] Models.LoginRequest loginRequest)
+        {
+            if (loginRequest == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid request body");
+            }
+
+            if (users.ContainsKey(loginRequest.Username) && users[loginRequest.Username] == loginRequest.Password)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Login successful" });
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Invalid credentials" });
+            }
+        }
+    }
+}
---
+++ BP-API/Models/SalesMisRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BP_API.Models
+{
+    public class SalesMisRecord
+    {
+        public int AssetNo { get; set; }
+        public string Phase { get; set; }
+        public string Building { get; set; }
+
+    }
+}
